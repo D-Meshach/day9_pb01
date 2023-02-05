e6f6119ee07471fa436758c0556bbdf4dc437d95
D-Meshach <meshachdaniel456@gmail.com>
2023-02-05 19:43:21 +0530

    Solving using Switch Case Statement

--- employeewage.sh
+++ employeewage.sh
@@ -2,10 +2,28 @@
 
 echo "Welcome To Employee Wage Computation Program on Master Branch";
 
-IS_PRESENT=$(($RANDOM%2));
 WAGE_PER_HR=20;
 IS_FULL_TIME=8;
 IS_PART_TIME=8;
 
+workingHour=0;
+totalSalary=0;
 
+for ((day=1; day<=20 ;day++))
+do
 
+	isPresent=$(($RANDOM%3));
+	case $isPresent in 
+			0)echo "Employee is absent";
+				workingHour=0;;
+			1)echo "Employee is present";
+				workingHour=8;;
+			2)echo "Employee is working part time";
+				workingHour=8;;
+	esac
+	salary=$(($WAGE_PER_HR * $workingHour));
+	totalSalary=$(($totalSalary + $salary));
+
+done
+
+echo "Employee has earned $totalSalary in a month"
