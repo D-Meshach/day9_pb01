96674d0d3ce8d3390b834a947f4aa016cf8fa8b4
D-Meshach <meshachdaniel456@gmail.com>
2023-02-05 20:15:02 +0530

    calculating wage for 20 days

--- employeewage.sh
+++ employeewage.sh
@@ -9,17 +9,19 @@ IS_PART_TIME=8;
 workingHour=0;
 totalSalary=0;
 
-isPresent=$(($RANDOM%3));
-case $isPresent in 
+for ((day=1; day<=20; day++))
+do
+	isPresent=$(($RANDOM%3));
+	case $isPresent in 
 			0)echo "Employee is absent";
 				workingHour=0;;
 			1)echo "Employee is present";
 				workingHour=8;;
 			2)echo "Employee is working part time";
 				workingHour=8;;
-esac
-salary=$(($WAGE_PER_HR * $workingHour));
-totalSalary=$(($totalSalary + $salary));
+	esac
+	salary=$(($WAGE_PER_HR * $workingHour));
+	totalSalary=$(($totalSalary + $salary));
 
-
-echo "Employee has earned $totalSalary in a month"
+done
+echo "Employee has earned $totalSalary in a month";
